c1ff02dee753f81e4b70de1b1daf4c0f04239f4b
MhmadRaziq <mhmd.abdraziq@gmail.com>
2023-07-05 12:37:19 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Mac on 05/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
